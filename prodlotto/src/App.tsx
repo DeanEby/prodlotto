f49c02028ec328d018af61283d4693c3d528f4af
DeanEby <ebyd21@gmail.com>
2025-02-28 20:26:25 -0500

    regenerate individual elements

--- prodlotto/src/App.tsx
+++ prodlotto/src/App.tsx
@@ -2,51 +2,75 @@ import { useEffect } from 'react';
 import './App.css'
 
 import {songElements} from './songElements.tsx'
-
+//import {Element} from './utils/data-elements.ts'
 
 function App() {
-  console.log(songElements);
+  let elements : string[] = [];
+  //console.log(songElements);
   function getRandomInt(max : number){
     return Math.floor(Math.random() * max)
   }
-  
-  function updateElements() {
-    const selectedCatagories = new Set<string []>();
-    while (selectedCatagories.size < 5) {
-      selectedCatagories.add(songElements[getRandomInt(songElements.length)]);
-    }
-    //console.log(selectedCatagories);
-    let selectedArrays = Array.from(selectedCatagories);
-    let newTitle = [];
-      for (let i = 0; i < selectedArrays.length; i++){
-        newTitle.push(selectedArrays[i][getRandomInt(selectedArrays[i].length)])
+
+  function updateElement(elementNumber : number){
+    let element = document.getElementById('element' + String(elementNumber));
+    let elementArray = songElements.get(elements[elementNumber - 1])
+      if (element && elementArray){
+        element.textContent = elementArray[getRandomInt(elementArray.length)];
       }
-    //let newTitleString = newTitle.toString()
-    //newTitleString = newTitleString.replace(/,/g , " | ")
-    let element1 = document.getElementById('element1');
-    if (element1){
-      element1.textContent = newTitle[0];
+  }
+  function updateElements() {
+    const selectedCatagories = new Set<string>();
+    let elementKeys = Array.from(songElements.keys())
+    while (selectedCatagories.size < 5) {
+      selectedCatagories.add(elementKeys[getRandomInt(elementKeys.length)]);
+    }
+    let selectedArray = Array.from(selectedCatagories);
+    elements = selectedArray;
+    for(let i=1; i < 6; i++){
+      updateElement(i);
     }
   }
-
   useEffect(() => {
     updateElements();
   });
+  let buttonClass = "bg-transparent hover:bg-purple-500 text-purple-700 font-semibold hover:text-white py-2 px-4 border border-purple-500 hover:border-transparent rounded scale-40"
   return (
-    <div className="justify-self-center items-center">
-      <div className = "flex  justify-between text-3xl p-2 font-bold text-gray-500 gap-50">
-        <div className=''>
+    <div className="justify-self-center items-center p-11 m-50">
+      <div className = "flex justify-between text-3xl p-5 font-bold text-gray-200 gap-50 capitalize .lg:box-border">
+        <div className='.grid-cols-5 .grid-rows-5'>
+          <button className={buttonClass}
+              onClick={() => updateElement(1)}>regenerate</button>
           <h1 id='element1'>testest</h1>
         </div>
-        <div className='flex gap-2'>
+        
+        <div className='.grid-cols-5 .grid-rows-5'>
+          <button className={buttonClass}
+              onClick={() => updateElement(2)}>regenerate</button>
           <h1 id='element2'>testest</h1>
         </div>
-        <h1 id='element3'>testest</h1>
-        <h1 id='element4'>testest</h1>
-        <h1 id='element5'>testest</h1>
+
+        <div className='.grid-cols-5 .grid-rows-5'>
+          <button className={buttonClass}
+              onClick={() => updateElement(3)}>regenerate</button>
+          <h1 id='element3'>testest</h1>
+        </div>
+
+        <div className='.grid-cols-5 .grid-rows-5'>
+          <button className={buttonClass}
+              onClick={() => updateElement(4)}>regenerate</button>
+          <h1 id='element4'>testest</h1>
+        </div>
+
+        <div className='.grid-cols-5 .grid-rows-5'>
+          <button className={buttonClass}
+              onClick={() => updateElement(5)}>regenerate</button>
+          <h1 id='element5'>testest</h1>
+        </div>
+
       </div>
-      <div>
-        <button className='bg-gray-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={() => updateElements()}>Generate Song Idea</button>
+      <div className="justify-self-center">
+        <button className='bg-transparent hover:bg-gray-100 text-purple-800 font-semibold py-2 px-4 border border-purple-400 rounded shadow'
+            onClick={() => updateElements()}>Generate Song Idea</button>
       </div>
     </div>
   )
--- prodlotto/src/songElements.tsx
+++ prodlotto/src/songElements.tsx
@@ -1,5 +1,5 @@
-export const songElements = [
-    [
+export let songElements = new Map<string, string[]>([
+    ["limitations", [
         "unconventional time signature",
         "microtonal",
         "no sidechain",
@@ -7,8 +7,8 @@ export const songElements = [
         "only nature samples",
         "no intro or outro",
         "made in under an hour"
-    ],
-    [
+    ]],
+    ["bpm", [
         "60 bpm", "61 bpm", "62 bpm", "63 bpm", "64 bpm",
         "65 bpm", "66 bpm", "67 bpm", "68 bpm", "69 bpm",
         "70 bpm", "71 bpm", "72 bpm", "73 bpm", "74 bpm",
@@ -38,8 +38,8 @@ export const songElements = [
         "190 bpm", "191 bpm", "192 bpm", "193 bpm", "194 bpm",
         "195 bpm", "196 bpm", "197 bpm", "198 bpm","199 bpm",
         "200 bpm", "Smallest possible bpm", "fastest possible bpm"
-    ],
-    [
+    ]],
+    ["key", [
         "a minor",
         "a# minor",
         "b minor",
@@ -53,8 +53,8 @@ export const songElements = [
         "g minor",
         "g# minor",
         "c major"
-    ],
-    [
+    ]],
+    ["instrument", [
         "guitar",
         "piano",
         "synth",
@@ -77,8 +77,8 @@ export const songElements = [
         "pitch bent synths",
         "pitch bent vocals",
         "pitch bent lead"
-    ],
-    [
+    ]],
+    ["production_technique", [
         "unconventional sidechain",
         "heavily distorted",
         "vocoded",
@@ -88,8 +88,8 @@ export const songElements = [
         "creative arpeggiation",
         "heavy sample use",
         "heavy automation"
-    ],
-    [
+    ]],
+    ["descriptive_word", [
         "sad",
         "happy",
         "melancholic",
@@ -106,8 +106,8 @@ export const songElements = [
         "upbeat",
         "danceable",
         "fast paced"
-    ],
-    [
+    ]],
+    ["theory", [
         "chordal",
         "harmonicly driven",
         "melodically driven",
@@ -126,7 +126,137 @@ export const songElements = [
         "kick driven",
         "baille drum pattern",
         "riff driven"
-    ]
-]
-    
-    
+    ]]
+])
+
+
+// export const songElements = [
+//     [
+//         "unconventional time signature",
+//         "microtonal",
+//         "no sidechain",
+//         "10 tracks or less",
+//         "only nature samples",
+//         "no intro or outro",
+//         "made in under an hour"
+//     ],
+//     [
+//         "60 bpm", "61 bpm", "62 bpm", "63 bpm", "64 bpm",
+//         "65 bpm", "66 bpm", "67 bpm", "68 bpm", "69 bpm",
+//         "70 bpm", "71 bpm", "72 bpm", "73 bpm", "74 bpm",
+//         "75 bpm", "76 bpm", "77 bpm", "78 bpm", "79 bpm",
+//         "80 bpm", "81 bpm", "82 bpm", "83 bpm", "84 bpm",
+//         "85 bpm", "86 bpm", "87 bpm", "88 bpm", "89 bpm",
+//         "90 bpm", "91 bpm", "92 bpm", "93 bpm", "94 bpm",
+//         "95 bpm", "96 bpm", "97 bpm", "98 bpm", "99 bpm",
+//         "100 bpm", "101 bpm", "102 bpm", "103 bpm", "104 bpm",
+//         "105 bpm", "106 bpm", "107 bpm", "108 bpm", "109 bpm",
+//         "110 bpm", "111 bpm", "112 bpm", "113 bpm", "114 bpm",
+//         "115 bpm", "116 bpm", "117 bpm", "118 bpm", "119 bpm",
+//         "120 bpm", "121 bpm", "122 bpm", "123 bpm", "124 bpm",
+//         "125 bpm", "126 bpm", "127 bpm", "128 bpm", "129 bpm",
+//         "130 bpm", "131 bpm", "132 bpm", "133 bpm", "134 bpm",
+//         "135 bpm", "136 bpm", "137 bpm", "138 bpm", "139 bpm",
+//         "140 bpm", "141 bpm", "142 bpm", "143 bpm", "144 bpm",
+//         "145 bpm", "146 bpm", "147 bpm", "148 bpm", "149 bpm",
+//         "150 bpm", "151 bpm", "152 bpm", "153 bpm", "154 bpm",
+//         "155 bpm", "156 bpm", "157 bpm", "158 bpm", "159 bpm",
+//         "160 bpm", "161 bpm", "162 bpm", "163 bpm", "164 bpm",
+//         "165 bpm", "166 bpm", "167 bpm", "168 bpm", "169 bpm",
+//         "170 bpm", "171 bpm", "172 bpm", "173 bpm", "174 bpm",
+//         "175 bpm", "176 bpm", "177 bpm", "178 bpm", "179 bpm",
+//         "180 bpm", "181 bpm", "182 bpm", "183 bpm", "184 bpm",
+//         "185 bpm", "186 bpm", "187 bpm", "188 bpm", "189 bpm",
+//         "190 bpm", "191 bpm", "192 bpm", "193 bpm", "194 bpm",
+//         "195 bpm", "196 bpm", "197 bpm", "198 bpm","199 bpm",
+//         "200 bpm", "Smallest possible bpm", "fastest possible bpm"
+//     ],
+//     [
+//         "a minor",
+//         "a# minor",
+//         "b minor",
+//         "c minor",
+//         "c# minor",
+//         "d minor",
+//         "d# minor",
+//         "e minor",
+//         "f minor",
+//         "f# minor",
+//         "g minor",
+//         "g# minor",
+//         "c major"
+//     ],
+//     [
+//         "guitar",
+//         "piano",
+//         "synth",
+//         "accoustic drums",
+//         "arpeggiated synths",
+//         "repetitive vocal chops",
+//         "breakbeat drums",
+//         "sampling",
+//         "tribal vocal samples",
+//         "big 808s",
+//         "8-bit synths",
+//         "orchestral elements",
+//         "big synths",
+//         "synthetic drums",
+//         "distorted synths",
+//         "foley elements",
+//         "marimba",
+//         "rave synths",
+//         "fm synth bass",
+//         "pitch bent synths",
+//         "pitch bent vocals",
+//         "pitch bent lead"
+//     ],
+//     [
+//         "unconventional sidechain",
+//         "heavily distorted",
+//         "vocoded",
+//         "microsampling",
+//         "heavy panning",
+//         "subtle panning",
+//         "creative arpeggiation",
+//         "heavy sample use",
+//         "heavy automation"
+//     ],
+//     [
+//         "sad",
+//         "happy",
+//         "melancholic",
+//         "ethereal",
+//         "expansive",
+//         "futuristic",
+//         "aggressive",
+//         "experimental",
+//         "synthetic",
+//         "hard hitting",
+//         "dusty",
+//         "disco",
+//         "psychedelic",
+//         "upbeat",
+//         "danceable",
+//         "fast paced"
+//     ],
+//     [
+//         "chordal",
+//         "harmonicly driven",
+//         "melodically driven",
+//         "high contrast",
+//         "vocal driven",
+//         "heavy call and response",
+//         "steady rhythm",
+//         "clearly defined sections",
+//         "swung",
+//         "strong downbeat",
+//         "mid-song switchup",
+//         "strong main element",
+//         "complementary guitar parts",
+//         "minimal",
+//         "evolving main synth",
+//         "kick driven",
+//         "baille drum pattern",
+//         "riff driven"
+//     ]
+// ]
